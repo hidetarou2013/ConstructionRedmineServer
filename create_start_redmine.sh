c08e43426c399b7a0ab559b1060b22d650d02aff
eithi2@IPTV-PC <hidetoshi_maekawa@e-it.co.jp>
2016-03-17 13:00:54 +0900

    refs #1 svn_storage_repo1 off

--- create_start_redmine.sh
+++ create_start_redmine.sh
@@ -3,7 +3,7 @@
 # pull method
 docker pull hidetarou2013/redmine-storage:latest 
 docker pull hidetarou2013/mysql-storage:latest 
-docker pull hidetarou2013/svn-storage:latest 
+#docker pull hidetarou2013/svn-storage:latest 
 docker pull sameersbn/redmine:latest 
 docker pull sameersbn/mysql:latest
 
@@ -19,9 +19,9 @@ docker create --name mysql-storage hidetarou2013/mysql-storage
 docker run --volumes-from mysql-storage --name=myredmine1_mysql -d -e 'DB_NAME=redmine_production'  -e 'DB_USER=redmine' -e 'DB_PASS=password' sameersbn/mysql:latest
 
 # svn
-docker create --name svn-storage hidetarou2013/svn-storage
-#docker run --name myredmine1_svn -t -d -v /home/vagrant/web-contents/:/var/www/html/ -p 80:80 -p 443:443 hidetarou2013/centos6-apache:SVN_repo1
-docker run --volumes-from svn-storage --name myredmine1_svn -t -d -v /home/vagrant/web-contents/:/var/www/html/ -p 80:80 -p 443:443 hidetarou2013/centos6-apache:SVN_repo1
+#docker create --name svn-storage hidetarou2013/svn-storage
+docker run --name myredmine1_svn -t -d -v /home/vagrant/web-contents/:/var/www/html/ -p 80:80 -p 443:443 hidetarou2013/centos6-apache:SVN_repo1
+#docker run --volumes-from svn-storage --name myredmine1_svn -t -d -v /home/vagrant/web-contents/:/var/www/html/ -p 80:80 -p 443:443 hidetarou2013/centos6-apache:SVN_repo1
 
 #docker run --name=redmine -it --rm -p 10080:80 --link myredmine1_mysql:mysql --volumes-from redmine-storage sameersbn/redmine
 #docker run --name=redmine -d -t -p 10080:80 --link myredmine1_mysql:mysql --volumes-from redmine-storage sameersbn/redmine
--- docker_stop_rm.sh
+++ docker_stop_rm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 docker stop `docker ps -qa` ; docker rm `docker ps -qa`
+docker ps -a
 
 exit 0
