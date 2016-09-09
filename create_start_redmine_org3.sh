89c212ad7d1c2be546bb57b599f1fb0cce700d74
maekawa@notepc2 <hidetoshi_maekawa@e-it.co.jp>
2016-09-09 12:58:43 +0900

    refs #1 create_start_redmine4.sh

--- create_start_redmine_org3.sh
+++ create_start_redmine_org3.sh
@@ -4,7 +4,8 @@
 docker pull hidetarou2013/redmine-storage:latest 
 docker pull hidetarou2013/mysql-storage:latest 
 #docker pull hidetarou2013/svn-storage:latest 
-docker pull sameersbn/redmine:latest 
+#docker pull sameersbn/redmine:latest 
+docker pull hidetarou2013/redmine:v3.3.0 
 docker pull sameersbn/mysql:latest
 
 # build method
@@ -13,6 +14,7 @@ docker pull sameersbn/mysql:latest
 #docker build -t hidetarou2013/mysql-storage github.com/hidetarou2013/mysql-storage
 #docker build -t hidetarou2013/redmine-storage github.com/hidetarou2013/redmine-storage
 #docker build -t hidetarou2013/svn-storage github.com/hidetarou2013/svn-storage
+docker build -t hidetarou2013/redmine:v3.3.0_a github.com/hidetarou2013/ConstructionRedmineServer
 
 docker create --name redmine-storage hidetarou2013/redmine-storage
 docker create --name mysql-storage hidetarou2013/mysql-storage
@@ -25,7 +27,8 @@ docker run --name myredmine1_svn -t -d -v /home/vagrant/web-contents/:/var/www/h
 
 #docker run --name=redmine -it --rm -p 10080:80 --link myredmine1_mysql:mysql --volumes-from redmine-storage sameersbn/redmine
 #docker run --name=redmine -d -t -p 10080:80 --link myredmine1_mysql:mysql --volumes-from redmine-storage sameersbn/redmine
-docker run --name=redmine -d -t -p 10080:80 --link myredmine1_mysql:mysql --link myredmine1_svn:svn --volumes-from redmine-storage sameersbn/redmine
+#docker run --name=redmine -d -t -p 10080:80 --link myredmine1_mysql:mysql --link myredmine1_svn:svn --volumes-from redmine-storage sameersbn/redmine
+docker run --name=myredmine1 -d -t -p 10080:80 --link myredmine1_mysql:mysql --link myredmine1_svn:svn --volumes-from redmine-storage hidetarou2013/redmine:v3.3.0_a
 
 # status check
 docker ps -a
